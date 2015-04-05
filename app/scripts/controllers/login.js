efd2d0e3c01d1161db93393cb1a2105885ae4714
loster719 <jan-niklaswortmann@gmx.net>
2015-04-05 16:15:12 +0200

    websocket connection

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,14 +8,47 @@
  * Controller of the pokerFrontendApp
  */
 angular.module('pokerFrontendApp')
-  .controller('LoginCtrl', function ($scope, ngFabForm, rest) {
+  .controller('LoginCtrl', function ($scope, ngFabForm, rest, $location) {
 
-    $scope.submit = function ()
-    {
-      rest.post("{'test':'test'}", function(data){
-        console.log(data);
-      });
+    var webSocket = new WebSocket(
+      'ws://192.168.0.139:8080/poker/websocket');
+
+
+    $scope.submit = function () {
+            console.log("submit");
+      webSocket.send(JSON.stringify({"body":{"user":"123456789"}, "event" : "login_user"}));
+
+
     };
     $scope.defaultFormOptions = ngFabForm.config;
+
+    webSocket.onerror = function (event) {
+      onError(event)
+    };
+
+    webSocket.onopen = function (event) {
+      onOpen(event)
+    };
+
+    webSocket.onmessage = function (event) {
+      onMessage(event)
+    };
+
+    function onMessage(event) {
+      console.log(event);
+      var data = JSON.parse(event.data);
+      if(data.status){
+        $location.path("/main")
+      }
+    }
+
+    function onOpen(event) {
+      console.log("onOpen", event);
+    }
+
+    function onError(event) {
+      console.log("onError",event);
+    }
+
 
   });
--- app/scripts/services/socket.js
+++ app/scripts/services/socket.js
@@ -11,7 +11,38 @@ angular.module('pokerFrontendApp')
   .service('socket', function ($websocket) {
     // AngularJS will instantiate a singleton by calling "new" on this function
 
-    var ws = $websocket.$new('ws://localhost:12345');
+    var webSocket = new WebSocket(
+      'ws://192.168.0.139:8080/poker/websocket');
 
+    webSocket.onerror = function (event) {
+      onError(event)
+    };
+
+    webSocket.onopen = function (event) {
+      onOpen(event)
+    };
+
+    webSocket.onmessage = function (event) {
+      onMessage(event)
+    };
+
+    function onMessage(event) {
+     console.log("12345", event)
+    }
+
+    function onOpen(event) {
+      document.getElementById('messages').innerHTML = 'Now Connection established';
+    }
+
+    function onError(event) {
+      alert(event.data);
+    }
+
+    function start() {
+      var text = document.getElementById("userinput").value;
+
+      webSocket.send(text);
+      return false;
+    }
 
   });
